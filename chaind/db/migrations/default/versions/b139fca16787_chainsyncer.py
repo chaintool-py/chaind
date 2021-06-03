fc193674231c4bf7976626beb70a58bc44b466b7
nolash <dev@holbrook.no>
2021-06-03 13:48:33 +0200

    initial commit

---
+++ chaind/db/migrations/default/versions/b139fca16787_chainsyncer.py
@@ -0,0 +1,28 @@
+"""chainsyncer
+
+Revision ID: b139fca16787
+Revises: 
+Create Date: 2021-06-03 13:09:23.731381
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+from chainsyncer.db.migrations.sqlalchemy import (
+        chainsyncer_upgrade,
+        chainsyncer_downgrade,
+        )
+
+# revision identifiers, used by Alembic.
+revision = 'b139fca16787'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    chainsyncer_upgrade()
+
+
+def downgrade():
+    chainsyncer_downgrade()
