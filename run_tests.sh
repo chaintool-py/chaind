e01fa4c54ab6f59b90e57634cd9d01f7a6e73caf
lash <dev@holbrook.no>
2022-03-14 21:41:00 +0000

    Add testrunner script

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set +e
+set +x
+default_pythonpath=$PYTHONPATH:.
+export PYTHONPATH=${default_pythonpath:-.}
+>&2 echo using pythonpath $PYTHONPATH
+for f in `ls tests/*.py`; do
+	python $f
+	if [ $? -gt 0 ]; then
+		exit 1
+	fi
+done
+set -x
+set -e
